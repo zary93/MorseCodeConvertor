0eae23b738c39e5b0335a17e5a2265ae8f6f9fa2
Zary93 <alex.zarioiu@yahoo.com>
2024-01-17 21:06:05 +0200

    Add a while function and a break condition

--- main.py
+++ main.py
@@ -15,16 +15,22 @@ MORSE_DIC = {'A': '.-', 'B': '-...',
              '(': '-.--.', ')': '-.--.-'}
 
 
-text = input("Write a text here: ")
-upper_text = text.upper()
-morse_message = []
-text_into_list = list(upper_text)
-for word in text_into_list:
-    if word == " ":
-        morse_word = "|"
-    else:
-        morse_word = MORSE_DIC[word]
-    morse_message.append(morse_word)
+def translate(word):
+    for word in text_into_list:
+        if word == " ":
+            morse_word = "|"
+        else:
+            morse_word = MORSE_DIC[word]
+        morse_message.append(morse_word)
+    print(morse_message)  # Return the text into a list
+    # print(''.join(morse_message)) # Return the text into a string
 
-print(morse_message)  # Return the text into a list
-# print(''.join(morse_message)) # Return the text into a string
+
+while True:
+    text = input("Write a text here: ")
+    upper_text = text.upper()
+    morse_message = []
+    text_into_list = list(upper_text)
+    if upper_text == "EXIT":
+        break
+    translate(upper_text)
